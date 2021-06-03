d6180c2e4075d2a30f87fdab47212004d4732377
Mridul2820 <immridulpanda2820@gmail.com>
2021-06-03 16:27:40 +0530

    connecting firbase and seed the data

---
+++ src/lib/firebase.js
@@ -0,0 +1,24 @@
+import Firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+// import { seedDatabase } from "../seed";
+
+const config = {
+    apiKey: process.env.REACT_APP_FIREBASE,
+    authDomain: "insta-clone-ab8df.firebaseapp.com",
+    projectId: "insta-clone-ab8df",
+    storageBucket: "insta-clone-ab8df.appspot.com",
+    messagingSenderId: "272516493158",
+    appId: "1:272516493158:web:d06325aa42b773f56e62a7",
+    measurementId: "G-ZCZCRS4S5E"
+}
+
+const firebase = Firebase.initializeApp(config);
+const { FieldValue } = Firebase.firestore;
+
+console.log('firebase', firebase);
+
+// seedDatabase(firebase)
+
+export { firebase, FieldValue };
