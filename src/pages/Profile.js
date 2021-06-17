6d0202a13bc2121629d93cd3ed4c03dc8777cca7
Mridul2820 <immridulpanda2820@gmail.com>
2021-06-18 01:16:44 +0530

    fetching user data in user profile page

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,11 +1,35 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
+import { useParams, useHistory } from 'react-router-dom'
+import { getUserByUserName } from '../services/firebase'
+import * as ROUTES from '../constants/routes'
 
 const Profile = () => {
-    return (
-        <div>
-            user
-        </div>
-    )
+    const { username } = useParams()
+    const history = useHistory()
+    const [user, setUser] = useState(null)
+    const [userExists, setUserExists] = useState(undefined)
+
+
+    useEffect(() => {
+        const checkUserExists = async() => {
+            const user = await getUserByUserName(username)
+
+            if(user.length > 0) {
+                setUser(user[0])
+                setUserExists(true)
+            }
+            else {
+                history.push(ROUTES.NOT_FOUND)
+            }
+        }
+
+        checkUserExists()
+        console.log('user', user);
+    }, [history, username])
+
+    return userExists ? (
+        <div>profile of {user.fullName}</div>
+    ) : null
 }
 
 export default Profile
--- src/services/firebase.js
+++ src/services/firebase.js
@@ -10,6 +10,18 @@ export async function doesUsernameExist(username) {
     return result.docs.length > 0;
 }
 
+export async function getUserByUserName(username) {
+    const result = await firebase
+        .firestore()
+        .collection('users')
+        .where('username', '==', username)
+        .get();
+  
+    return result.docs.map(item => ({
+        ...item.data(),
+        docId: item.id
+    }))
+}
 
 // get user from the firestore where userId === userId (passed from the auth)
 export async function getUserByUserId(userId) {
